e9b5041fbe245c6b503fb37e30f998c452f7ad68
sudharsfinale <sudharsfinale@gmail.com>
2025-05-13 17:28:17 +0530

    Complete portfolio

---
+++ src/constants/index.js
@@ -0,0 +1,282 @@
+const navLinks = [
+  {
+    name: "Work",
+    link: "#work",
+  },
+  {
+    name: "Experience",
+    link: "#experience",
+  },
+  {
+    name: "Skills",
+    link: "#skills",
+  },
+  {
+    name: "Testimonials",
+    link: "#testimonials",
+  },
+];
+
+const words = [
+  { text: "Ideas", imgPath: "/images/ideas.svg" },
+  { text: "Concepts", imgPath: "/images/concepts.svg" },
+  { text: "Designs", imgPath: "/images/designs.svg" },
+  { text: "Code", imgPath: "/images/code.svg" },
+  { text: "Ideas", imgPath: "/images/ideas.svg" },
+  { text: "Concepts", imgPath: "/images/concepts.svg" },
+  { text: "Designs", imgPath: "/images/designs.svg" },
+  { text: "Code", imgPath: "/images/code.svg" },
+];
+
+const counterItems = [
+  { value: 2, suffix: "+", label: "Years of Experience" },
+  { value: 10, suffix: "+", label: "Satisfied Clients" },
+  { value: 5, suffix: "+", label: "Completed Projects" },
+  { value: 90, suffix: "%", label: "Client Retention Rate" },
+];
+
+const logoIconsList = [
+  {
+    imgPath: "/images/logos/company-logo-1.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-2.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-3.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-4.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-5.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-6.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-7.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-8.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-9.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-10.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-11.png",
+  },
+];
+
+const abilities = [
+  {
+    imgPath: "/images/seo.png",
+    title: "Quality Focus",
+    desc: "Delivering high-quality results while maintaining attention to every detail.",
+  },
+  {
+    imgPath: "/images/chat.png",
+    title: "Reliable Communication",
+    desc: "Keeping you updated at every step to ensure transparency and clarity.",
+  },
+  {
+    imgPath: "/images/time.png",
+    title: "On-Time Delivery",
+    desc: "Making sure projects are completed on schedule, with quality & attention to detail.",
+  },
+];
+
+const techStackImgs = [
+  {
+    name: "React Developer",
+    imgPath: "/images/logos/react.png",
+  },
+  {
+    name: "Python Developer",
+    imgPath: "/images/logos/python.svg",
+  },
+  {
+    name: "Backend Developer",
+    imgPath: "/images/logos/node.png",
+  },
+  {
+    name: "Interactive Developer",
+    imgPath: "/images/logos/three.png",
+  },
+  {
+    name: "Project Manager",
+    imgPath: "/images/logos/git.svg",
+  }
+];
+
+const techStackIcons = [
+  {
+    name: "React Developer",
+    modelPath: "/models/react_logo-transformed.glb",
+    scale: 1,
+    rotation: [0, 0, 0],
+  },
+  {
+    name: "React Native Developer",
+    modelPath: "/models/react_native_logo-transformed.glb",
+    scale: 1,
+    rotation: [0, 0, 0],
+  },
+  {
+    name: "Backend Developer",
+    modelPath: "/models/node-transformed.glb",
+    scale: 5,
+    rotation: [0, -Math.PI / 2, 0],
+  },
+  {
+    name: "Interactive Developer",
+    modelPath: "/models/three.js-transformed.glb",
+    scale: 0.05,
+    rotation: [0, 0, 0],
+  },
+  {
+    name: "Project Manager",
+    modelPath: "/models/git-svg-transformed.glb",
+    scale: 0.05,
+    rotation: [0, -Math.PI / 4, 0],
+  }
+];
+
+const expCards = [
+  {
+    review: "Sudharsan brought creativity and technical expertise to the team, significantly improving our frontend performance. His work has been invaluable in delivering faster experiences.",
+    imgPath: "/images/wp_logo.svg",
+    logoPath: "/images/whatsplan_logo.webp",
+    title: "Senior Software Engineer",
+    date: "February 2025 - Present",
+    responsibilities: [
+      "Developed and maintained user-facing features for our product website.",
+      "Collaborated closely with UI/UX designers to ensure seamless user experiences.",
+      "Optimized web applications for maximum speed and scalability.",
+    ],
+  },
+  {
+    review: "Sudharsan's contributions to Whatsplan web applications have been outstanding. He approaches challenges with a problem-solving mindset.",
+    imgPath: "/images/wp_logo.svg",
+    logoPath: "/images/whatsplan_logo.webp",
+    title: "Software Engineer",
+    date: "November 2023 - January 2025",
+    responsibilities: [
+      "Led the development of Docker's web applications, focusing on scalability.",
+      "Worked with backend engineers to integrate APIs seamlessly with the frontend.",
+      "Contributed to open-source projects that were used with the Docker ecosystem.",
+    ],
+  },
+  {
+    review: "Sudharsan’s work on Rehlte mobile app brought a high level of quality and efficiency. He delivered solutions that enhanced our mobile experience & meet our product goals.",
+    imgPath: "/images/wp_logo.svg",
+    logoPath: "/images/whatsplan_logo.webp",
+    title: "Software Engineering Test Intern",
+    date: "July 2023 - November 2023",
+    responsibilities: [
+      "Built cross-platform mobile apps using React Native, integrating with PHP's backend services.",
+      "Improved app performance and user experience through code optimization and testing.",
+      "Coordinated with the product team to implement features based on feedback.",
+    ],
+  },
+];
+
+const expLogos = [
+  {
+    name: "logo1",
+    imgPath: "/images/logo1.png",
+  },
+  {
+    name: "logo2",
+    imgPath: "/images/logo2.png",
+  },
+  {
+    name: "logo3",
+    imgPath: "/images/logo3.png",
+  },
+];
+
+const testimonials = [
+  {
+    name: "Hema Latha",
+    mentions: "@WhatsPlan",
+    review:
+      "Sudharsan is a skilled front-end developer with expertise in React, Redux, and Next.js. Hardworking and creative, he takes initiative to learn backend development and quickly grasps project requirements. A reliable team player with strong communication skills, Sudharsan consistently delivers clean, efficient code and brings positive energy to every project.",
+    imgPath: "/images/client1.png",
+  },
+  {
+    name: "Muthu Yonisha R",
+    mentions: "@Eloiacs",
+    review:
+      "My experience with Sudharsan was very good . He has lots of problem solving skills and open minded with me. He has more knowledge in new learning skills and taught me also . He has faced lots of multitasking projects and he finished at a time. I have a great experience with him and thanks a lot Sudharsan that you collaborate with me.😇😊",
+    imgPath: "/images/client3.png",
+  },
+  {
+    name: "Daniel Joshua",
+    mentions: "@Paypal",
+    review:
+      "Collaborating with Sudharsan was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Sudharsan's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Sudharsan is the ideal partner.",
+    imgPath: "/images/client2.png",
+  },
+  {
+    name: "Aishwarya",
+    mentions: "@WhatsPlan",
+    review:
+      "Working with Sudharsan has made a noticeable difference in how smoothly our projects run. You’re someone who takes ownership and always brings solutions instead of problems. I know I can trust you to deliver on time and still think two steps ahead. It’s rare to find someone so dependable and sharp",
+    imgPath: "/images/client5.png",
+  },
+  {
+    name: "Ganesh",
+    mentions: "@WhatsPlan",
+    review:
+      "Bro, your JS game is solid. Half the time I’m watching you debug something, I’m learning new tricks myself. You’re fast, clean, and always think about scalability. Also, you never gatekeep knowledge, which makes the whole team better. Honestly, you’re one of the best teammates I’ve worked with.",
+    imgPath: "/images/client4.png",
+  },
+  {
+    name: "Prathiba",
+    mentions: "@Whatsplan",
+    review:
+      "Sudharsan make design-development collaboration so easy. It’s clear he care about how things look and feel, not just how they work. Whether it’s pixel-perfect implementation or responsive fixes, he always take the time to get it right. It’s a pleasure working with someone who values UX like he do.",
+    imgPath: "/images/client6.png",
+  },
+];
+
+const socialImgs = [
+  {
+    name: "insta",
+    imgPath: "/images/insta.png",
+    imgLink: "https://www.instagram.com/sudharsfinale/"
+  },
+  // {
+  //   name: "fb",
+  //   imgPath: "/images/x.png",
+  //   imgLink: "https://www.linkedin.com/in/sudharsan4012/"
+  // },
+  {
+    name: "x",
+    imgPath: "/images/logos/github_1.png",
+    imgLink: "https://github.com/sudharsfinale"
+  },
+  {
+    name: "linkedin",
+    imgPath: "/images/linkedin.png",
+    imgLink: "https://www.linkedin.com/in/sudharsan4012/"
+  },
+];
+
+export {
+  words,
+  abilities,
+  logoIconsList,
+  counterItems,
+  expCards,
+  expLogos,
+  testimonials,
+  socialImgs,
+  techStackIcons,
+  techStackImgs,
+  navLinks,
+};
---
+++ src/sections/Footer.jsx
@@ -0,0 +1,31 @@
+import { socialImgs } from "../constants";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="flex flex-col justify-center">
+          <p>Terms & Conditions</p>
+        </div>
+        <div className="socials">
+          {socialImgs.map((socialImg, index) => (
+            <div
+              onClick={() => window.open(socialImg?.imgLink, "_blank")}
+              key={index}
+              className="icon"
+            >
+              <img src={socialImg.imgPath} alt="social icon" />
+            </div>
+          ))}
+        </div>
+        <div className="flex flex-col justify-center">
+          <p className="text-center md:text-end">
+            © {new Date().getFullYear()} Sudharsan. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
